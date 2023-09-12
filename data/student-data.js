b3fec90b617017ffb2d4a75e0953853975baa5d3
Tuan Nguyen <tuan.nat915@gmail.com>
2023-09-12 16:56:25 -0400

    Complete intro-express-lab

---
+++ data/student-data.js
@@ -0,0 +1,7 @@
+const students = [
+  { name: "Tuan Nguyen", gender: "Male", register: true },
+  { name: "Chi Hoang", gender: "Female", register: false },
+  { name: "Mia Nguyen", gender: "Female", register: false }
+]
+
+export { students }
---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import * as studentsData from "./data/student-data.js";
+
+// create Express app
+const app = express()
+
+// configure the app (app.set)
+app.set("view engine", "ejs")
+
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+app.get("/", (req, res) => {
+  res.redirect("/home")
+})
+app.get("/home", (req, res) => {
+  res.render("home")
+})
+
+app.get("/students", (req, res) => {
+  res.render("students/index", { students: studentsData.students })
+})
+
+
+// tell the app to listen on port 3000
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
